5913a9a5a4843f9c62734cdcb32d093183ad2951
jchodon <jigcho27@gmail.com>
2023-12-14 18:12:01 -0500

    content script

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
+//listen to any update in tab system
+//find the tab we are on currently and see if it is youtube
+
+// @ts-ignore
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+  }
+});
